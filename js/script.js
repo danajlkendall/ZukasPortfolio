d0993b6acd14bc5d65162cae5a18f7af5bfc45d5
Dana Zukas <dana.zukas@gmail.com>
2017-01-21 09:58:08 -0500

    JavaScript Added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function upper()
+{
+var uc = document.getElementById('input1').value;
+document.getElementById('input1').value = uc.toUpperCase();
+}
+
+function lower()
+{
+var lc = document.getElementById('input1').value;
+document.getElementById('input1').value = lc.toLowerCase();
+}
